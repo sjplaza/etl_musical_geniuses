13ab7b476af0ab1a249b98bbf022d2a6f5033ff7
Stephanie Plaza <66174042+sjplaza@users.noreply.github.com>
2020-11-11 22:38:05 -0600

    updating file

--- Project2/app.js
+++ Project2/app.js
@@ -1,197 +1,35 @@
-// set the dimensions and margins of the graph
-let margin = { top: 20, right: 40, bottom: 80, left: 100 },
-    width = 600 - margin.left - margin.right,
-    height = 450 - margin.top - margin.bottom;
 
-// append the svg object to the body of the page
-let svg = d3.select("#my_dataviz")
-    .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-//Read the data
-d3.csv("./data_files/steph_years.csv", function (data) {
+var svg = d3.select("svg"),
+    margin = 200,
+    width = svg.attr("width") - margin,
+    height = svg.attr("height") - margin;
 
-    // Add X axis
-    let x = d3.scaleLinear()
-        .domain([0, 100])
-        .range([0, width]);
-    svg.append("g")
+
+var xScale = d3.scaleBand().range([0, width]).padding(0.4),
+    yScale = d3.scaleLinear().range([height, 0]);
+
+var g = svg.append("g")
+    .attr("transform", "translate(" + 100 + "," + 100 + ")");
+
+d3.csv("filtered_data.csv", function (error, data) {
+    if (error) {
+        throw error;
+    }
+    xScale.domain(data.map(function (d) { return d.year; }));
+    yScale.domain([0, d3.max(data, function (d) { return d.value; })]);
+
+    g.append("g")
         .attr("transform", "translate(0," + height + ")")
-        .call(d3.axisBottom(x).ticks(5));
+        .call(d3.axisBottom(xScale));
 
-    // Add X axis label:
-    svg.append("text")
-        .attr("text-anchor", "end")
-        .attr("x", width)
-        .attr("y", height + 50)
-        .text("Measurement");
-
-    // Add Y axis
-    let y = d3.scaleLinear()
-        .domain([1940, 1970])
-        .range([height, 0]);
-    svg.append("g")
-        .call(d3.axisLeft(y).ticks(5));
-
-    // Add Y axis label:
-    svg.append("text")
-        .attr("text-anchor", "end")
-        .attr("x", 0)
-        .attr("y", 0)
-        .text("Year")
-        .attr("text-anchor", "start")
-
-    // Add a scale for bubble size
-    let z = d3.scaleSqrt()
-        .domain([0, 1000])
-        .range([2, 30]);
-
-    // Add a scale for bubble color
-    let myColor = d3.scaleOrdinal()
-        .domain(["Dance", "Energy", "Speech", "Tempo"])
-        .range(d3.schemeSet1);
-
-
-    // -1- Create a tooltip div that is hidden by default:
-    var tooltip = d3.select("#my_dataviz")
-        .append("div")
-        .style("opacity", 0)
-        .attr("class", "tooltip")
-        .style("background-color", "black")
-        .style("border-radius", "5px")
-        .style("padding", "10px")
-        .style("color", "white")
-
-    // -2- Create 3 functions to show / update (when mouse move but stay on same circle) / hide the tooltip
-    var showTooltip = function (d) {
-        tooltip
-            .transition()
-            .duration(200)
-        tooltip
-            .style("opacity", 1)
-            .html("Year: " + d.year)
-            .style("left", (d3.mouse(this)[0] + 30) + "px")
-            .style("top", (d3.mouse(this)[1] + 30) + "px")
-    }
-    var moveTooltip = function (d) {
-        tooltip
-            .style("left", (d3.mouse(this)[0] + 30) + "px")
-            .style("top", (d3.mouse(this)[1] + 30) + "px")
-    }
-    var hideTooltip = function (d) {
-        tooltip
-            .transition()
-            .duration(200)
-            .style("opacity", 0)
-    }
-
-
-    // What to do when one group is hovered
-    var highlight = function (d) {
-        // reduce opacity of all groups
-        d3.selectAll(".bubbles").style("opacity", .05)
-        // expect the one that is hovered
-        d3.selectAll("." + d).style("opacity", 1)
-    }
-
-    // And when it is not hovered anymore
-    var noHighlight = function (d) {
-        d3.selectAll(".bubbles").style("opacity", 1)
-    }
-
-
-    // Add dots
-    svg.append('g')
-        .selectAll("dot")
-        .data(data)
-        .enter()
-        .append("circle")
-        .attr("class", function (d) { return "bubbles " + d.year })
-        .attr("cx", function (d) { return x(d.danceability); })
-        .attr("cy", function (d) { return y(d.speechiness); })
-        .attr("r", function (d) { return z(d.tempo); })
-        .style("fill", function (d) { return myColor(d.year); })
-        // -3- Trigger the functions for hover
-        .on("mouseover", showTooltip)
-        .on("mousemove", moveTooltip)
-        .on("mouseleave", hideTooltip)
-
-
-    // Add legend: circles
-    var valuesToShow = [200, 200, 200]
-    var xCircle = 390
-    var xLabel = 450
-    svg
-        .selectAll("legend")
-        .data(valuesToShow)
-        .enter()
-        .append("circle")
-        .attr("cx", xCircle)
-        .attr("cy", function (d) { return height - 100 - z(d) })
-        .attr("r", function (d) { return z(d) })
-        .style("fill", "none")
-        .attr("stroke", "black")
-
-    // Add legend: segments
-    svg
-        .selectAll("legend")
-        .data(valuesToShow)
-        .enter()
-        .append("line")
-        .attr('x1', function (d) { return xCircle + z(d) })
-        .attr('x2', xLabel)
-        .attr('y1', function (d) { return height - 100 - z(d) })
-        .attr('y2', function (d) { return height - 100 - z(d) })
-        .attr('stroke', 'black')
-        .style('stroke-dasharray', ('2,2'))
-
-    // Add legend: labels
-    svg
-        .selectAll("legend")
-        .data(valuesToShow)
-        .enter()
+    g.append("g")
+        .call(d3.axisLeft(yScale).tickFormat(function (d) {
+            return "$" + d;
+        }).ticks(10))
         .append("text")
-        .attr('x', xLabel)
-        .attr('y', function (d) { return height - 100 - z(d) })
-        .text(function (d) { return d / 100 })
-        .style("font-size", 10)
-        .attr('alignment-baseline', 'middle')
-
-    // Legend title
-    svg.append("text")
-        .attr('x', xCircle)
-        .attr("y", height - 100 + 30)
-        .text("Do we need??")
-        .attr("text-anchor", "middle")
-
-    // Add one dot in the legend for each name.
-    var size = 20
-    var allgroups = ["Dance", "Energy", "Speech", "Tempo"]
-    svg.selectAll("myrect")
-        .data(allgroups)
-        .enter()
-        .append("circle")
-        .attr("cx", 425)
-        .attr("cy", function (d, i) { return 10 + i * (size + 5) }) // 100 is where the first dot appears. 25 is the distance between dots
-        .attr("r", 10)
-        .style("fill", function (d) { return myColor(d) })
-        .on("mouseover", highlight)
-        .on("mouseleave", noHighlight)
-
-    // Add labels beside legend dots
-    svg.selectAll("mylabels")
-        .data(allgroups)
-        .enter()
-        .append("text")
-        .attr("x", 425 + size * .8)
-        .attr("y", function (d, i) { return i * (size + 5) + (size / 2) }) // 100 is where the first dot appears. 25 is the distance between dots
-        .style("fill", function (d) { return myColor(d) })
-        .text(function (d) { return d })
-        .attr("text-anchor", "left")
-        .style("alignment-baseline", "middle")
-        .on("mouseover", highlight)
-        .on("mouseleave", noHighlight)
-})
+        .attr("y", 6)
+        .attr("dy", "0.71em")
+        .attr("text-anchor", "end")
+        .text("value");
+});
